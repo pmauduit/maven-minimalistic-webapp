62d61fc33108adf3f290187c769876b80f5ffe65
Pierre Mauduit <pierre.mauduit@camptocamp.com>
2020-02-14 17:57:10 +0100

    fiddling with javax sessions
    Possibility to pass a X-Set-Name: blah http header to set a name into
    the session.
    
    (the idea is to scale the minimalistic webapp and see if I can share
    session across multiple instances of the same webapp spanned into
    several different servlet containers).

--- src/main/java/fr/spironet/minimalistic/MinServlet.java
+++ src/main/java/fr/spironet/minimalistic/MinServlet.java
@@ -7,9 +7,12 @@ import java.util.Enumeration;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import java.net.InetAddress;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 public class MinServlet extends HttpServlet {
@@ -19,14 +22,26 @@ public class MinServlet extends HttpServlet {
 	@Override
 	public void service(HttpServletRequest req,
 			HttpServletResponse res) throws IOException {
+
 		res.setHeader("Content-Type", "text/plain");
+
 		OutputStream os = res.getOutputStream();
+
+		os.write("-- servletContextName: ".getBytes());
+		os.write(req.getServletContext().getServletContextName().getBytes());
+		os.write("\n".getBytes());
 
 		// dumping received http headers
 		os.write("-- HTTP Headers received:\n".getBytes());
 		Enumeration<String> hns = req.getHeaderNames();
 		while (hns.hasMoreElements()) {
 			String currentHeader = hns.nextElement();
+
+			if (currentHeader.equalsIgnoreCase("X-Set-Name")) {
+				HttpSession sess = req.getSession();
+				sess.setAttribute("name", req.getHeader(currentHeader));
+			}
+
 			Enumeration<String> currentValue = req.getHeaders(currentHeader);
 			List<String> values = new ArrayList<String>();
 			while (currentValue.hasMoreElements()) {
@@ -35,6 +50,22 @@ public class MinServlet extends HttpServlet {
 			os.write(String.format("%s:\t%s\n", currentHeader,
 					values.stream().collect(Collectors.joining(","))).getBytes());
 		}
+
+		if (req.getSession().getAttribute("name") != null) {
+			os.write(String.format("Name in session: %s\n",
+		  	req.getSession().getAttribute("name")).getBytes());
+		}
+
+		try {
+			  InetAddress lhost = InetAddress.getLocalHost();
+				os.write(String.format("Hostname: %s\n",
+				lhost.getHostName()).getBytes());
+		} catch (Exception e) {
+			// who cares
+		}
+
+
+
 	}
 
 }
